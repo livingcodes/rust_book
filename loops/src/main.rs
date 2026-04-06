cb7751491909db171fe02724278177a56444fc41
Casey <case.joyce@gmail.com>
2026-04-05 22:44:03 -0500

    for loop

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,13 +1,7 @@
 fn main() {
-  let mut i = 0;
-  let result = loop {
-    i = i + 1;
-    println!("i:{i}");
-
-    if i == 10 {
-      break i * 2;
-    }
-  };
-
-  println!("result:{result}");
+  let a = [1, 2, 3];
+  
+  for num in a {
+    println!("{}", num);
+  }
 }
