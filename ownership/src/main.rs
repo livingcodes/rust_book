640d33f365ae97c9fb49c06889edd107df060099
Casey <case.joyce@gmail.com>
2026-04-12 21:31:43 -0500

    ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,5 +1,23 @@
-use std::io;
 fn main() {
+  let x = String::from("hello");
+  //let y = x; // will cause compile-time error
+  println!("{x}");
+}
+
+/* fn main() {
+  let mut x = String::from("hello");
+  use_x(x);
+
+  // compile-time error: can't use x after move (ie passed as arg)
+  x.push_str(" world");
+}
+
+fn use_x(x:String) {
+  println!("x is {}", x);
+} */
+
+//use std::io;
+/* fn main_str() {
   // string literal
   let s1 = "hello world";
   println!("string literal: {}", s1);
@@ -18,4 +36,4 @@ fn main() {
   s3.push_str(" ");
   s3.push_str(&ln);
   println!("mutable string: hello {}", s3);
-}
+} */
