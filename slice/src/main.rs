49c086599a24c01fc9b43688c5b0b295d64c2e57
Casey <case.joyce@gmail.com>
2026-04-15 23:14:14 -0500

    slice

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -1,17 +1,6 @@
 fn main() {
   let s = String::from("hello world");
-  let space_after_first_word_index = first_word(&s);
-  println!("end {}", &space_after_first_word_index);
-}
-
-fn first_word(s: &String) -> usize {
-  let bytes = s.as_bytes();
-
-  for (i, &item) in bytes.iter().enumerate() {
-    if item == b' ' {
-      return i;
-    }
-  }
-
-  return s.len();
+  let hello = &s[0..5];
+  let world = &s[6..11];
+  println!("{hello} {world}");
 }
