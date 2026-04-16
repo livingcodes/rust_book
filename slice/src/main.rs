9efab6bd0f0e7e3ee6a95ffd112c697cffed2bd2
Casey <case.joyce@gmail.com>
2026-04-15 23:40:48 -0500

    slice: string ref is a slice

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
-  let s = String::from("hello world");
-  let word = first_word(&s);
-  println!("the first word is {}", word);
+  let s_literal : &str = "hello world"; // string literal is a slice
+  let word = first_word(&s_literal);
+  println!("the first word is {word}");
+
+  let s_string : String = String::from("hello world");
+  let word = first_word(&s_string); // reference to String is also a slice
+  println!("the first word is {word}");
 }
 
-fn first_word(s :&String) -> &str {
+fn first_word(s :&str) -> &str {
   for (i, &ch) in s.as_bytes().iter().enumerate() {
     if ch == b' ' {
       return &s[..i];
