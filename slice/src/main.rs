7c7832cb15c23e29e10dff7b6c4019a3e841220d
Casey <case.joyce@gmail.com>
2026-04-15 23:23:47 -0500

    slice alt

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -3,4 +3,15 @@ fn main() {
   let hello = &s[0..5];
   let world = &s[6..11];
   println!("{hello} {world}");
+
+  let x = String::from("hello");
+  let lo = &x[3..];
+  println!("{lo}");
+
+  let len = x.len();
+  let lo = &x[3..len];
+  println!("{lo} {len}");
+
+  let lo = &x[3..5];
+  println!("{lo}");
 }
