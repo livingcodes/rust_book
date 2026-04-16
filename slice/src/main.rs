5437da9f8f660d134aa3077fa5cfb72c1034ddbf
Casey <case.joyce@gmail.com>
2026-04-15 23:44:44 -0500

    slice int array

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -6,6 +6,10 @@ fn main() {
   let s_string : String = String::from("hello world");
   let word = first_word(&s_string); // reference to String is also a slice
   println!("the first word is {word}");
+
+  let arr = [1, 2, 3, 4, 5];
+  let slice = &arr[1..3];
+  assert_eq!(slice, &[2, 3]);
 }
 
 fn first_word(s :&str) -> &str {
