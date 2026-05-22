07c39107b905e46e8c9636401d7d02dfea7c64a4
Casey <case.joyce@gmail.com>
2026-05-21 22:24:09 -0500

    index out of bounds

--- panic/src/main.rs
+++ panic/src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
-  println!("hello world");
-  panic!("Test panic");
-  println!("goodbye");
+  let v = vec![1, 2, 4, 8];
+  println!("{}", v[7]); // index out of bounds
 }
+
+// fn main_panic() {
+//   println!("hello world");
+//   panic!("Test panic");
+//   println!("goodbye");
+// }
